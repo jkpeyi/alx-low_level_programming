4647bada32ddec88debb4d0fd2c3f2763f7e1d17
jkpeyi <jkpeyi@gmail.com>
2022-12-11 07:57:27 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - prints a string to stderr
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+return (1);
+}
