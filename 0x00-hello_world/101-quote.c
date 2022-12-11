362030bb3c1d616cd0ffdec35e55bc8debf4a653
jkpeyi <jkpeyi@gmail.com>
2022-12-11 08:08:16 -0800

    101 corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,22 @@
-#include <unistd.h>
-/**
- * main - prints a string to stderr
- *
- * Return: 1
- */
 
+#include <unistd.h>
+
+
+/**
+ * main - Prints a string to the standard error
+ *
+ * Return:  1 (Success)
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int i = 0;
+
+while (s[i])
+{
+if (write(2, &s[i], 1) == -1)
+return (1);
+i++;
+}
 return (1);
 }
