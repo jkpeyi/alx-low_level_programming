d374d9831912777a5776ec1bc4678f107bba4015
jkpeyi <jkpeyi@gmail.com>
2022-12-14 09:03:48 -0800

    #include main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * Return: 0
+ *  **/
+
+int main(void)
+{
+
+char *str = "_putchar\n";
+
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+
+}
+}
