27b7c9e0a871b3448698c7602a2a9b6f1471115f
jkpeyi <jkpeyi@gmail.com>
2022-12-19 07:31:43 -0800

    push.sh

---
+++ 0x01-variables_if_else_while/push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# add all files to staging area
+git add .
+
+# get the name of the newest file in the current directory
+newest_file=$(ls -t | head -1)
+
+# commit the changes with the name of the newest file as the commit message
+git commit -m "$newest_file"
+
+# push the changes to the remote repository
+git push
+
+# print a success message
+echo "Changes successfully pushed to the repository!
