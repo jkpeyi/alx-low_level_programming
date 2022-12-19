f55bafd31ec033dc14f12e3444253541e6e1d7a0
jkpeyi <jkpeyi@gmail.com>
2022-12-19 07:35:42 -0800

    push.sh

--- 0x01-variables_if_else_while/push.sh
+++ 0x01-variables_if_else_while/push.sh
@@ -8,7 +8,7 @@ newest_file=$(ls -t | head -1)
 
 # commit the changes with the name of the newest file as the commit message
 git commit -m "$newest_file"
-
+echo $newest_file
 # push the changes to the remote repository
 git push
 
