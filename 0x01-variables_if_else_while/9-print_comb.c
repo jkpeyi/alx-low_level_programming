33f0f69b8b467551b482f48c6376e80808fb88b8
jkpeyi <jkpeyi@gmail.com>
2022-12-27 00:17:02 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Ascending comma separated digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+for (int i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
+
