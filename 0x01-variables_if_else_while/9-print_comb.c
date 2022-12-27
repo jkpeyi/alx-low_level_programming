056e4b55ba580919872a484429628692032d2e28
jkpeyi <jkpeyi@gmail.com>
2022-12-27 00:26:28 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,18 @@
  */
 int main(void)
 {
-int i;
-for (i = 0; i <= 9; i++)
+
+for (int i = 0; i <= 9; i++)
 {
 putchar(i + '0');
 if (i != 9)
+{
 putchar(',');
 putchar(' ');
 }
+else
 putchar('\n');
+}
 return (0);
 }
 
